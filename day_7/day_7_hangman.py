db09311e2627199870cf5ea8667c1dba9be71547
Kera Cudmore <kera.cudmore@hotmail.com>
2023-09-26 21:21:13 +0100

    add: day 7 project part 1

---
+++ day_7/day_7_hangman.py
@@ -0,0 +1,26 @@
+"""Day 7 Project: Hangman"""
+
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1
+# Randomly choose a word from the word_list and assign it to a variable
+# called chosen_word.
+
+chosen_word = random.choice(word_list)
+
+# TODO-2
+# Ask the user to guess a letter and assign their answer to a variable
+# called guess. Make guess lowercase.
+
+guess = input("Guess a letter\n").lower()
+
+# TODO-3
+# Check if the letter the user guessed (guess) is one of the letters
+# in the chosen_word.
+
+if guess in chosen_word:
+    print(f"Congratulations! {guess} is in the word")
+else:
+    print(f"Sorry, {guess} isn't in the word")
